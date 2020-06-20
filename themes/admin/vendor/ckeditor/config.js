dab7dec371fe9843cccc544bd3329111e57bff6b
hecland <hecland@qq.com>
2020-06-20 17:49:24 +0800

    改进 CKEditor

--- themes/admin/vendor/ckeditor/config.js
+++ themes/admin/vendor/ckeditor/config.js
@@ -1,11 +1,9 @@
 /**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-  // console.log('CKEDITOR.editorConfig');
-
   // 不清除空的 i 标签和 span 标签
   CKEDITOR.dtd.$removeEmpty.i = 0;
   CKEDITOR.dtd.$removeEmpty.span = 0;
@@ -26,21 +24,27 @@ CKEDITOR.editorConfig = function( config ) {
   };
 
   config.toolbarGroups = [
-      { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-      { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-      { name: 'styles', groups: [ 'styles' ] },
-      { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-      { name: 'links', groups: [ 'links' ] },
-      { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-      { name: 'insert', groups: [ 'insert' ] },
-      { name: 'forms', groups: [ 'forms' ] },
-      { name: 'tools', groups: [ 'tools' ] },
-      // [ 'name': 'others', 'groups': [ 'others' ] ],
-      // [ 'name': 'editing', 'groups': [ 'find', 'selection', 'spellchecker', 'editing' ] ],
-      // [ 'name': 'colors', 'groups': [ 'colors' ] ],
-      // [ 'name': 'about', 'groups': [ 'about' ] ],
-  ];
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'document', groups: [ 'doctools', 'mode', 'document' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
 
-  config.removeButtons = 'Underline,Styles,Strike,Italic,Indent,Outdent,Blockquote,About,SpecialChar,HorizontalRule,Scayt,Cut,Copy,Paste,PasteText,PasteFromWord';
+	config.removeButtons = 'Cut,Copy,Paste,Underline,Strike,Undo,Redo,searchCode,CommentSelectedRange,UncommentSelectedRange';
 
+  config.extraPlugins = 'codemirror';
+
+  config.codemirror = {
+    // Set this to the theme you wish to use (codemirror themes)
+    theme: 'material',
+  };
 };
