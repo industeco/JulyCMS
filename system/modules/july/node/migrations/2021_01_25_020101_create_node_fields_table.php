91ffc4d643dcf63eb471704b2508573e68f5932e
hecland <hecland@qq.com>
2021-02-24 16:00:58 +0800

    恢复 create_node_fields_table migration

--- system/modules/july/node/migrations/2021_01_25_020101_create_node_fields_table.php
+++ system/modules/july/node/migrations/2021_01_25_020101_create_node_fields_table.php
@@ -21,8 +21,7 @@ class CreateNodeFieldsTable extends Migration
             $table->string('id')->primary();
 
             // 字段类型
-            // $table->string('field_type_id');
-            $table->string('field_type');
+            $table->string('field_type_id');
 
             // 是否预设：
             //  - 不可删除
@@ -33,41 +32,41 @@ class CreateNodeFieldsTable extends Migration
             // 是否全局字段：全局字段会在新建模型时默认出现在全局字段列表
             $table->boolean('is_global')->default(false);
 
+            // 分组标题
+            $table->string('group_title')->nullable();
+
+            // 搜索权重
+            $table->unsignedTinyInteger('search_weight')->default(0);
+
+            // 建议长度
+            $table->unsignedSmallInteger('maxlength')->default(0);
+
             $table->string('label');
             $table->string('description')->nullable();
 
-            // 分组标题
-            $table->string('group_title')->nullable();
+            // 是否必填
+            $table->boolean('is_required')->default(false);
+
+            // 表单字段下方输入提示
+            $table->string('helpertext')->nullable();
+
+            // 默认值
+            $table->string('default_value')->nullable();
+
+            // 可选值
+            $table->string('options')->nullable();
+
+            // 验证规则
+            $table->string('rules')->nullable();
+
+            // 字段占位符
+            $table->string('placeholder')->nullable();
 
             // 初始语言版本
             $table->string('langcode', 12);
 
             // 时间戳
             $table->timestamps();
-
-            // // 搜索权重
-            // $table->unsignedTinyInteger('search_weight')->default(0);
-
-            // // 建议长度
-            // $table->unsignedSmallInteger('maxlength')->default(0);
-
-            // // 是否必填
-            // $table->boolean('is_required')->default(false);
-
-            // // 表单字段下方输入提示
-            // $table->string('helpertext')->nullable();
-
-            // // 默认值
-            // $table->string('default_value')->nullable();
-
-            // // 可选值
-            // $table->string('options')->nullable();
-
-            // // 验证规则
-            // $table->string('rules')->nullable();
-
-            // // 字段占位符
-            // $table->string('placeholder')->nullable();
         });
 
         // 填充数据
